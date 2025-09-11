38a7b0f879b30aff017896158f91783db40806eb
Danilo Malovic <danilomalovic@hotmail.com>
2025-09-11 13:46:46 -0400

    style: clean scripts and accent logo

--- script.js
+++ script.js
@@ -1,37 +1,37 @@
 const navSlide = () => {
-    const burger = document.querySelector('.burger');
-    const nav = document.querySelector('.nav-links');
+  const burger = document.querySelector('.burger');
+  const nav = document.querySelector('.nav-links');
 
-    burger.addEventListener('click', () => {
-        nav.classList.toggle('nav-active');
-        burger.classList.toggle('toggle');
-    });
-}
+  burger.addEventListener('click', () => {
+    nav.classList.toggle('nav-active');
+    burger.classList.toggle('toggle');
+  });
+};
 
 const smoothScroll = () => {
-    const navLinks = document.querySelectorAll('.nav-links a');
+  const navLinks = document.querySelectorAll('.nav-links a');
 
-    navLinks.forEach(link => {
-        link.addEventListener('click', (e) => {
-            e.preventDefault();
-            const href = link.getAttribute('href');
-            const offsetTop = document.querySelector(href).offsetTop;
+  navLinks.forEach((link) => {
+    link.addEventListener('click', (e) => {
+      e.preventDefault();
+      const href = link.getAttribute('href');
+      const offsetTop = document.querySelector(href).offsetTop;
 
-            scroll({
-                top: offsetTop,
-                behavior: 'smooth'
-            });
+      scroll({
+        top: offsetTop,
+        behavior: 'smooth',
+      });
 
-            // Close nav on link click
-            const nav = document.querySelector('.nav-links');
-            const burger = document.querySelector('.burger');
-            if (nav.classList.contains('nav-active')) {
-                nav.classList.remove('nav-active');
-                burger.classList.remove('toggle');
-            }
-        });
+      // Close nav on link click
+      const nav = document.querySelector('.nav-links');
+      const burger = document.querySelector('.burger');
+      if (nav.classList.contains('nav-active')) {
+        nav.classList.remove('nav-active');
+        burger.classList.remove('toggle');
+      }
     });
-}
+  });
+};
 
 navSlide();
 smoothScroll();
